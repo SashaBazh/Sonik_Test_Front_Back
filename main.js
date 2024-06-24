83adc3eb1890e29ed43a6c7e4ccd7475146c18e6
Sashaaasssss <bazhenovsasha17052006@gmail.com>
2024-06-25 00:02:48 +0300

    http_server

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import { Telegraf, Markup } from "telegraf"
 
 const token = '7289282616:AAFDnBhtAm6zxoEjPKxM_SrWMfUecX3mqBk'
-const webAppUrl = ''
+const webAppUrl = 'http://94.26.250.142:3000';
 
 const bot = new Telegraf(token)
 
